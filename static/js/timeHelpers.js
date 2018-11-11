61cda023a63f218f19fedb97b1be7e77b59feb2e
Paul Durack <pdurack@gmail.com>
2018-11-11 03:39:58 +0000

    fixed available bike stands

--- static/js/timeHelpers.js
+++ static/js/timeHelpers.js
@@ -13,13 +13,13 @@ function time_convert(num){
   var hours = Math.floor(num / 60);
   var minutes = num % 60;
   if(hours == 0){
-    converted = minutes + 'mins';
+    converted = minutes + ' mins';
     return converted;
   } else if (hours == 1){
-      converted = hours + 'hr' + ' ' + minutes + 'mins';
+      converted = hours + ' hr' + ' ' + minutes + ' mins';
       return converted
   } else {
-      converted = hours + 'hrs' + ' ' + minutes + 'mins';
+      converted = hours + ' hrs' + ' ' + minutes + ' mins';
       return converted
   }
 }
