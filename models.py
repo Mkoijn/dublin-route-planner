bc4c4853be7625bc976bfb4b486ee58668711c0d
Paul Durack <pdurack@gmail.com>
2018-11-29 23:18:41 +0000

    db

--- models.py
+++ models.py
@@ -4,9 +4,10 @@ Author: Paul Durack
 """
 
 from app import db
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
