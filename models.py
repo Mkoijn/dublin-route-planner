3beb9d9fb1cec4e87e0ff199b2ff2f53f2cff40c
Paul Durack <pdurack@gmail.com>
2018-11-29 23:00:59 +0000

    remove usermixin

--- models.py
+++ models.py
@@ -4,10 +4,9 @@ Author: Paul Durack
 """
 
 from app import db
-from flask_login import UserMixin
 
 
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
