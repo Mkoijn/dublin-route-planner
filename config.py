08a6faa7864301dd1b1c9c39f277eee66346b9fc
Paul Durack <pdurack@gmail.com>
2018-11-29 20:39:04 +0000

    Setting up postgres db

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 SECRET_KEY = 'SecretKey'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Paulinho70@localhost:5432/pre-registration'
 SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
 DEBUG = True
