23887c9128681744e2381c1a00d466024740e469
Paul Durack <pdurack@gmail.com>
2018-12-13 14:14:22 +0000

    config

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ SECRET_KEY = 'SecretKey'
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Paulinho70@localhost:5432/pre-registration'
 SQLALCHEMY_DATABASE_URI = 'postgres://tgycqibxifstwg:b7c640523721c4f7ce1900c6ee97f6a7446e62e5a52872e6b2db55f82d204c94@ec2-46-137-95-47.eu-west-1.compute.amazonaws.com:5432/d47drd0a60rb1u'
 SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
-DEBUG = True
+DEBUG = False
